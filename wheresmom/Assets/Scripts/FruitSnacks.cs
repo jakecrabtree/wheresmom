2bbb6dbc143d635eee9f5c1fb79cef5e2f12d9a9
Jake <jakercrabtree@utexas.edu>
2019-01-26 12:12:20 -0600

    fruit snacks almost

--- wheresmom/Assets/Scripts/FruitSnacks.cs
+++ wheresmom/Assets/Scripts/FruitSnacks.cs
@@ -4,18 +4,36 @@ using UnityEngine;
 
 public class FruitSnacks : Interactable
 {
-    // Start is called before the first frame update
-    void Start(){
-        
-    }
 
-    // Update is called once per frame
-    void Update(){
-        
-    }
+    private static float increaseWalkSpeed = 10;
+    private static float increaseRunSpeed = 10;
+    private static float initialDuration = 1;
+    private static float decayDuration = 2; 
+
+    bool consumed = false;
 
     public override void Interact(GameObject interactor){
-       // GameManager.Instance.PlayerObj.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().
+        if (!consumed){
+            consumed = true;
+            StartCoroutine("SugarRush");
+            GetComponent<Renderer>().enabled = false;
+        }
+    }
+
+    void Update(){
+
+    }
+
+    private IEnumerator SugarRush(){
+        UnityStandardAssets.Characters.FirstPerson.FirstPersonController controller = GameManager.Instance.PlayerObj.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>();
+        controller.IncreaseSpeeds(increaseRunSpeed, increaseWalkSpeed);
+        yield return new WaitForSeconds(initialDuration);
+        for (float i = 0; i <= decayDuration; i+= Time.deltaTime){
+            //Mathf.Lerp(0, increaseRunSpeed, );
+            controller.DecreaseSpeeds(Time.deltaTime, Time.deltaTime);
+            yield return null;
+        }
         Destroy(gameObject);
+        yield return null;
     }
 }
