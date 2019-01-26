54576e2641e2e375e7f49f8b2c1ce2155494f7ee
danielw <daniel.wong@utexas.edu>
2019-01-26 01:40:27 -0600

    gps energy

---
+++ wheresmom/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	private static GameManager instance;
+
+	public static GameManager Instance {
+		get {
+			return instance;
+		}
+	}
+	Player player;
+	Mom mom;
+	void Awake () {
+		if(instance != null && instance != this) {
+			Destroy(this.gameObject);
+		} else {
+			instance = this;
+		}
+		mom = GameObject.FindGameObjectWithTag("Mom").GetComponent<Mom>();
+		player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+	}
+
+	public Player PlayerObj {
+		get {
+			return player;
+		}
+	}
+
+	public Mom MomObj {
+		get {
+			return mom;
+		}
+	}	
+
+}
