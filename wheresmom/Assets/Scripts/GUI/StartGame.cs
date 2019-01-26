e936bcb82ce21a6c14b24f7bed0ea8675823f33c
danielw <daniel.wong@utexas.edu>
2019-01-26 13:05:07 -0600

    main menu, start, pause screen, and more

---
+++ wheresmom/Assets/Scripts/GUI/StartGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+	public Button start;
+	// Use this for initialization
+	void Start () {
+		start = GetComponent<Button>();
+		start.onClick.AddListener(OpenGame);
+	}
+	
+	void OpenGame() {
+		SceneManager.LoadScene("Game");
+	}
+}
