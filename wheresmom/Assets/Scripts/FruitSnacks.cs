9457ae35faca34ebfd82d9e251ad9042ba1be875
Jake <jakercrabtree@utexas.edu>
2019-01-26 13:07:54 -0600

    gui elements

--- wheresmom/Assets/Scripts/FruitSnacks.cs
+++ wheresmom/Assets/Scripts/FruitSnacks.cs
@@ -9,8 +9,13 @@ public class FruitSnacks : Interactable
     private static float increaseRunSpeed = 10;
     private static float initialDuration = 1;
     private static float decayDuration = 2; 
+    private static float decayStep = 0.1f;
 
     bool consumed = false;
+
+    void Start(){
+        guiFileName = "fruitsnacks";
+    }
 
     public override void Interact(GameObject interactor){
         if (!consumed){
@@ -28,10 +33,10 @@ public class FruitSnacks : Interactable
         UnityStandardAssets.Characters.FirstPerson.FirstPersonController controller = GameManager.Instance.PlayerObj.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>();
         controller.IncreaseSpeeds(increaseRunSpeed, increaseWalkSpeed);
         yield return new WaitForSeconds(initialDuration);
-        for (float i = 0; i <= decayDuration; i+= Time.deltaTime){
-            //Mathf.Lerp(0, increaseRunSpeed, );
-            controller.DecreaseSpeeds(Time.deltaTime, Time.deltaTime);
-            yield return null;
+        float multiplier = decayStep/decayDuration;
+        for (float i = 0; i <= decayDuration; i+= decayStep){
+            controller.DecreaseSpeeds(multiplier*increaseRunSpeed, multiplier*increaseWalkSpeed);
+            yield return new WaitForSeconds(decayStep);
         }
         Destroy(gameObject);
         yield return null;
--- wheresmom/Assets/Scripts/Interactable.cs
+++ wheresmom/Assets/Scripts/Interactable.cs
@@ -1,8 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public abstract class Interactable : MonoBehaviour
 {
+    protected string guiFileName;
+    private string guiFolderName = "interact_gui";
     public abstract void Interact(GameObject interactor);
+
+    public string getGuiFileName(){
+        return (guiFileName == null) ? guiFileName : guiFolderName + Path.DirectorySeparatorChar + guiFileName;
+    }
 }
