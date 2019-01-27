317a2145c9fd4f948b26e807c8fa663901848643
danielw <daniel.wong@utexas.edu>
2019-01-27 03:25:57 -0600

    stuff

---
+++ wheresmom/Assets/Scripts/GUI/HelpPop.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HelpPop : MonoBehaviour
+{
+	public Button help;
+	// Use this for initialization
+	void Start () {
+		help = GetComponent<Button>();
+		help.onClick.AddListener(Help);
+	}
+	
+	void Help() {
+		SceneManager.LoadScene("Help");
+	}
+}
