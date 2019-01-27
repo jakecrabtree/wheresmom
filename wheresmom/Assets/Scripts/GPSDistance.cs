0e8b6b205cb579519b80067ec1f229fd60969a87
danielw <daniel.wong@utexas.edu>
2019-01-27 09:24:24 -0600

    gps

--- wheresmom/Assets/Scripts/GPSDistance.cs
+++ wheresmom/Assets/Scripts/GPSDistance.cs
@@ -5,21 +5,28 @@ using UnityEngine.UI;
 
 public class GPSDistance : MonoBehaviour
 {
-    Text score;
+    Material m_Material;
     // Start is called before the first frame update
     void Start()
     {
-        score = GetComponent<Text>();
+        m_Material = GetComponent<Renderer>().material;
     }
 
     // Update is called once per frame
     void Update()
     {
         Player player = GameManager.Instance.PlayerObj;
-        if(!player.on) {
-            score.text = "";
-        } else { 
-            score.text = (int) Mathf.Floor(player.momDistance()) + " m";
+        int dist = (int) Mathf.Floor(player.momDistance());
+        if(player.on) {
+            if(dist > 40) {
+                m_Material.color = new Color(0.3366411f, 0.8396226f, 0.7878978f, 0.951569f);
+            } else if(dist > 20) {
+                m_Material.color = new Color(1f, 0.4172136f, 0f, 0.95f);
+            } else {
+                m_Material.color = new Color(1f, 0.03f, 0f, 0.95f);
+            }
+        } else {
+            m_Material.color = new Color(0.6745283f, .67f, 0.67f, 0.5f);
         }
     }
 }
